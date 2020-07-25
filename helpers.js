1119890301a26f673856aa4ad71210fde5525054
tangibledream <darclan28@yahoo.com>
2020-07-24 21:45:58 -0500

    cleaned up the code a little more

--- helpers.js
+++ helpers.js
@@ -9,7 +9,7 @@ const getY = index => { return Math.floor(index / 8) + 1 }
 
 const isOdd = num => { return num % 2 != 0 }
 
-const squareColor = (index) => {
+const squareColor = index => {
    return (isOdd(getY(index)) ? isOdd(getX(index)) ? "white" : "black" : isOdd(getX(index)) ? "black" : "white");
 }
 
