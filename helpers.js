971dd5f33c13f761135c40298092db503923a465
tangibledream <darclan28@yahoo.com>
2020-09-26 22:19:01 -0500

    remade pieces into an imported array

--- helpers.js
+++ helpers.js
@@ -11,15 +11,7 @@ const getY = index => { return Math.floor(index / 8) + 1 }
 
 const isOdd = num => { return num % 2 != 0 }
 
-const piecesJSON = () => {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'pieces.json', false);
-  //xhr.responseType = 'text';
-  xhr.send();
-  return xhr.responseText;
-}
-
 const squareColor = index => {
    return ((isOdd(getY(index)) === isOdd(getX(index))) ? "white" : "black");
 }
-export { flipValue, getX, getY, piecesJSON, squareColor }
+export { flipValue, getX, getY, squareColor }
